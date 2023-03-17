7c5fa207cef833a4b4c48d61996108fcf52ebb35
matdenkas <64288827+matdenkas@users.noreply.github.com>
2023-03-17 01:54:13 -0500

    Fixing degreeToRadians in Utility.ts

--- src/Utility.ts
+++ src/Utility.ts
@@ -1,28 +1,28 @@
 import { Vec2 } from "./Vec2"
 
-function cartesianToRadian(cartesianCords: Vec2) {
+export function cartesianToRadian(cartesianCords: Vec2) {
     return Math.tan(cartesianCords.y / cartesianCords.x);
 }
 
-function radianToDegree(radians: number) {
-    return radians * 180 / Math.PI;
+export function radianToDegree(radians: number) {
+    return radians * (180 / Math.PI);
 }
 
-function cartesianToDegree(cartesianCords: Vec2) {
+export function cartesianToDegree(cartesianCords: Vec2) {
     return radianToDegree(cartesianToRadian(cartesianCords));
 }
 
-function degreesToRadians(degrees: number) {
-    return degrees * (180 / Math.PI);
+export function degreesToRadians(degrees: number) {
+    return degrees * (Math.PI / 180);
 }
 
-function radianToCartesian(radians: number) {
+export function radianToCartesian(radians: number) {
     return new Vec2(
         Math.cos(radians),
         Math.sin(radians)
     );
 }
 
-function degreesToCartesian(degrees: number) {
+export function degreesToCartesian(degrees: number) {
     return radianToCartesian(degreesToRadians(degrees));
 }
